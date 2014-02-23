73111671d1a515f9a4bd586350e2788d7ac92c08
Vicky Chijwani <vickychijwani@gmail.com>
2014-02-23 23:05:07 +0530

    Retry Metascore-fetching for different platforms, for cases where the game is not rated / has a different name on the first platform.

--- Gimmick/src/main/java/io/github/vickychijwani/gimmick/api/Metacritic.java
+++ Gimmick/src/main/java/io/github/vickychijwani/gimmick/api/Metacritic.java
@@ -11,6 +11,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
@@ -28,39 +29,46 @@ public class Metacritic {
     private static final String SCORE = "score";
 
     public static void fetchMetascore(@NotNull Game game) {
-        Log.i(TAG, "Fetching Metascore for \"" + game.name + "\"");
-
         String url = BASE_URL;
-        Platform platform = game.getPlatforms().next();
-        Map<String, String> paramsMap = new HashMap<String, String>();
-        paramsMap.put("title", game.name);
-        paramsMap.put("platform", String.valueOf(platform.getMetacriticId()));
-        JSONObject params = new JSONObject(paramsMap);
 
-        RequestFuture<JSONObject> future = RequestFuture.newFuture();
-        JsonObjectRequest req = new JsonObjectRequest(url, params, future, future) {
-            @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
-                Map<String, String> headers = new HashMap<String, String>();
-                headers.put("X-Mashape-Authorization", API_KEY);
-                return headers;
+        Iterator<Platform> platforms = game.getPlatforms();
+        while (platforms.hasNext()) {
+            Platform platform = platforms.next();
+            Map<String, String> paramsMap = new HashMap<String, String>();
+            paramsMap.put("title", game.name);
+            paramsMap.put("platform", String.valueOf(platform.getMetacriticId()));
+            JSONObject params = new JSONObject(paramsMap);
+
+            Log.i(TAG, "Fetching Metascore for \"" + game.name + "\" (" + platform.getShortName() + ")");
+
+            RequestFuture<JSONObject> future = RequestFuture.newFuture();
+            JsonObjectRequest req = new JsonObjectRequest(url, params, future, future) {
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    Map<String, String> headers = new HashMap<String, String>();
+                    headers.put("X-Mashape-Authorization", API_KEY);
+                    return headers;
+                }
+            };
+
+            NetworkRequestQueue.add(req);
+
+            try {
+                JSONObject resultJson = future.get().getJSONObject(RESULT);
+                game.metascore = Short.parseShort(resultJson.getString(SCORE));
+                return;     // metascore fetched successfully, break loop and return
+            } catch (InterruptedException e) {
+                Log.e(TAG, Log.getStackTraceString(e));
+            } catch (ExecutionException e) {
+                Log.e(TAG, Log.getStackTraceString(e));
+            } catch (JSONException e) {
+                Log.e(TAG, Log.getStackTraceString(e));
+            } catch (NumberFormatException ignored) {
+
             }
-        };
-
-        NetworkRequestQueue.add(req);
-
-        try {
-            JSONObject resultJson = future.get().getJSONObject(RESULT);
-            game.metascore = Short.parseShort(resultJson.getString(SCORE));
-        } catch (InterruptedException e) {
-            Log.e(TAG, Log.getStackTraceString(e));
-        } catch (ExecutionException e) {
-            Log.e(TAG, Log.getStackTraceString(e));
-        } catch (JSONException e) {
-            Log.e(TAG, Log.getStackTraceString(e));
-        } catch (NumberFormatException ignored) {
-
         }
+
+        Log.i(TAG, "Metascore not found for \"" + game.name + "\" on any platform (" + game.getPlatformsDisplayString() + ")");
     }
 
     public static void setApiKey(@NotNull String apiKey) {
